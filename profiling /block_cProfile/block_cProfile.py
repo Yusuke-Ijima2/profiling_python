e74792aed1b8f1310944c4aae4ef71e1135c0a8e
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2023-04-27 12:40:49 +0900

    コメント追加

--- profiling /block_cProfile/block_cProfile.py
+++ profiling /block_cProfile/block_cProfile.py
@@ -1,70 +1,48 @@
-import linecache
-import line_profiler
+import cProfile
+import pstats
+from io import StringIO
+import importlib.util
 
 
-# 指定されたファイル名と行番号からソースコードを取得する関数
-def get_source_code(filename, lineno):
-    return linecache.getline(filename, lineno).strip()
+# source_code.pyファイルをインポートする関数
+def import_source_code(file_path):
+    spec = importlib.util.spec_from_file_location("source_code", file_path)
+    source_code = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(source_code)
+    return source_code
 
 
-# 与えられたソースコードとターゲット関数を用いて、行ごとの実行時間を計測する関数
-def line_profiling(source_code, target_functions):
-    # line_profilerオブジェクトを作成
-    lp = line_profiler.LineProfiler()
+# ソースコードのプロファイリングを行い、結果を表示する関数
+def profile_code(source_code):
+    pr = cProfile.Profile()
+    pr.enable()  # プロファイリングを開始
+    source_code.main()
+    pr.disable()  # プロファイリングを終了
 
-    # ターゲット関数をプロファイラに追加
-    for func in target_functions:
-        lp.add_function(func)
+    s = StringIO()
+    ps = pstats.Stats(pr, stream=s).sort_stats("cumulative")
 
-    # プロファイリングを開始
-    lp.enable_by_count()
-    lp.run(source_code)
-    lp.disable_by_count()
+    # カスタム書式で出力を整形
+    format_str = "{:<10} {:<10} {:<10} {:<10} {:<10} {}"
+    print(format_str.format("ncalls", "tottime", "percall",
+          "cumtime", "percall", "filename:lineno(function)"))
+    print("-" * 80)
 
-    # プロファイリング結果を取得
-    profile_data = lp.get_stats()
+    for func in ps.fcn_list:
+        cc, nc, tt, ct, callers = ps.stats[func]
+        ncalls = f"{nc} ({cc})" if nc != cc else str(nc)
+        tottime = f"{tt:0.6f}"
+        percall = f"{tt/nc:0.6f}"
+        cumtime = f"{ct:0.6f}"
+        percall2 = f"{ct/cc:0.6f}"
+        file, lineno, func_name = func
 
-    results = {}
-    # プロファイリング結果から関数ごとの実行時間を取得
-    for (function_tuple, timings) in profile_data.timings.items():
-        # 各行の実行時間を取得
-        for (line, nhits, time) in timings:
-            source_line = get_source_code(function_tuple[0], line)
-            results.setdefault(
-                line, {"source_line": source_line, "nhits": 0, "time": 0})
-            results[line]["nhits"] += nhits
-            results[line]["time"] += time
-
-    return results
+        print(format_str.format(ncalls, tottime, percall,
+              cumtime, percall2, f"{file}:{lineno}({func_name})"))
 
 
-# 実行時間プロファイリングの対象となるサンプル関数1
-def example_function():
-    total = 0
-    for i in range(10):
-        total += i
-    return total
-
-
-# 実行時間プロファイリングの対象となるサンプル関数2
-def another_function():
-    total = 0
-    for i in range(20):
-        total += i
-    return total
-
+source_code_file = "source_code.py"
+source_code = import_source_code(source_code_file)
 
 if __name__ == "__main__":
-    # プロファイリング対象のソースコード
-    sample_code = "example_function()\nanother_function()"
-
-    # 行ごとの実行時間プロファイリングを実行
-    profiling_results = line_profiling(
-        sample_code, [example_function, another_function])
-
-    # プロファイリング結果を出力
-    for line, result in profiling_results.items():
-        # Convert to microseconds
-        avg_time_per_hit = (result['time'] / result['nhits'])
-        print(
-            f"{result['source_line']}: nhits={result['nhits']}, time={avg_time_per_hit:.2f}マイクロ秒")
+    profile_code(source_code)
