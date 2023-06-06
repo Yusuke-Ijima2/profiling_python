63afbbd0224a3075eb8fbc44c78051d104603444
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2023-06-06 15:57:10 +0900

    あ

--- ast/list_comprehension/before/analysis/test_code/12.py
+++ ast/list_comprehension/before/analysis/test_code/12.py
@@ -4,7 +4,10 @@ import os
 
 # URLからHTMLを取得しパースする
 page_url = 'URL'
-r = requests.get(page_url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+r = requests.get(page_url, headers=headers)
 soup = BeautifulSoup(r.text, 'html.parser')
 
 # imgタグを全て取得し、src属性の値を取得、リストに格納
