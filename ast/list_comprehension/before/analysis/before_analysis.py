91c3a91decc4e6215e90953594d58f0a7cd73fbc
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2023-06-16 15:01:59 +0900

    6/16

--- ast/list_comprehension/before/analysis/before_analysis.py
+++ ast/list_comprehension/before/analysis/before_analysis.py
@@ -2,14 +2,43 @@ import os
 import ast
 
 
-class ListComprehensionPotentialFinder(ast.NodeVisitor):
+class ParentNodeTrackingAstVisitor(ast.NodeVisitor):
     def __init__(self):
         super().__init__()
+        self.parent_tracker = []  # 訪れたノードの親ノードを追跡するためのリスト
+
+    def visit(self, node):
+        super().visit(node)
+        if self.parent_tracker and self.parent_tracker[-1] is node:
+            self.parent_tracker.pop()
+
+    def generic_visit(self, node):
+        self.parent_tracker.append(node)
+        super().generic_visit(node)
+        if self.parent_tracker[-1] is node:
+            self.parent_tracker.pop()
+
+
+# ast.NodeVisitorを継承したクラスを作成します。このクラスは、抽象構文木を訪れるためのツールとなります。
+class ListComprehensionPotentialFinder(ParentNodeTrackingAstVisitor):
+    def __init__(self):
+        super().__init__()
+        self.parent = None
+        # potentialフラグは、リスト内包表記に変換可能なforループを見つけた場合にTrueとなります。
         self.potential = False
+        # if-elif構造の存在を検出するフラグを追加します。
         self.has_elif = False
         self.has_break = False
         self.has_mismatched_continue = False
         self.has_non_append_call = False
+        self.has_map_lambda = False
+
+    def visit_Call(self, node):
+        # lambda,mapの検出
+        if isinstance(node.func, ast.Name) and node.func.id == 'map':
+            if len(node.args) > 0 and isinstance(node.args[0], ast.Lambda):
+                self.has_map_lambda = True
+        self.generic_visit(node)
 
     def visit_Break(self, node):
         self.has_break = True
@@ -59,16 +88,16 @@ class ListComprehensionPotentialFinder(ast.NodeVisitor):
                     # if文内に一つのステートメントだけがある場合を確認します。
                     if len(statement.body) == 1:
                         if_body_statement = statement.body[0]
-                        # ステートメントがappendメソッドの呼び出しだった場合、potentialフラグを    Trueにします。
+                        # ステートメントがappendメソッドの呼び出しだった場合、potentialフラグをTrueにします。
                         if self.is_append_call(if_body_statement):
                             self.potential = True
-                # ステートメントがif文でなく、appendメソッドの呼び出しだった場合、potentialフラグを    Trueにします。
+                # ステートメントがif文でなく、appendメソッドの呼び出しだった場合、potentialフラグをTrueにします。
                 elif self.is_append_call(statement):
                     self.potential = True
-                # ステートメントが副作用を持つ可能性のある関数やメソッドの呼び出し（例：print）である    場合、potentialフラグをFalseにします。
-                elif isinstance(statement, ast.Expr) and isinstance(statement.value, ast.    Call):
+                # ステートメントが副作用を持つ可能性のある関数やメソッドの呼び出し（例：print）である場合、potentialフラグをFalseにします。
+                elif isinstance(statement, ast.Expr) and isinstance(statement.value, ast.Call):
                     self.potential = False
-        # whileループ内にさらにforループやif文があるかもしれないため、それをチェックするために    generic_visitを呼び出します。
+        # whileループ内にさらにforループやif文があるかもしれないため、それをチェックするためにgeneric_visitを呼び出します。
         self.generic_visit(node)
 
     def is_append_call(self, statement):
@@ -87,7 +116,7 @@ def has_potential_for_list_comprehension(filepath):
     finder = ListComprehensionPotentialFinder()
     finder.visit(tree)
 
-    return finder.potential and not finder.has_elif and not finder.has_break and not finder.has_mismatched_continue and not finder.has_non_append_call
+    return finder.potential and not finder.has_elif and not finder.has_break and not finder.has_mismatched_continue and not finder.has_non_append_call or finder.has_map_lambda
 
 
 directory = '/Users/iijimayuusuke/Profile_yusuke_ijima/ast/list_comprehension/before/analysis/test_code'
